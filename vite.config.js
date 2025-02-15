887738c17252622384d81b135d2cc9508f327ba0
ilyas-hub <91405460+ilyas-hub@users.noreply.github.com>
2025-02-15 19:48:30 +0530

    Fix Render deployment issue: allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173, // Default Vite preview port
+    strictPort: true,
+    host: true,
+    allowedHosts: ['cryptodashboard-jt16.onrender.com'], // Add Render host
+  }
 })
